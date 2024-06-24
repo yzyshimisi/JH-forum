41ee70590379392a3ef6218820bc2c74cf13142f
xixiibn5100 <1584914306@qq.com>
2024-06-24 23:37:31 +0800

    fix(request):修复request文件的类型错误

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,53 +1,48 @@
-import axios, { AxiosRequestConfig, AxiosRequestHeaders } from 'axios';
+import axios, { AxiosRequestConfig, AxiosRequestHeaders, Method } from 'axios';
 
 const service = axios.create({
-    baseURL: import.meta.env.VITE_HOST as string,
+    baseURL: import.meta.env.VITE_HOST,
     timeout: 30000,
 });
 
-// 请求拦截器
+// 请求拦截器，用于添加鉴权头部
 service.interceptors.request.use(
     (config) => {
-        // 鉴权Header
+        console.log('请求配置:', config);
         const token = localStorage.getItem('PAOPAO_TOKEN');
         if (token) {
-            if (!config.headers) {
-                config.headers = {} as AxiosRequestHeaders;
-            }
-            config.headers['Authorization'] = `Bearer ${token}`;
+            config.headers = {
+                ...config.headers,
+                'Authorization': `Bearer ${token}`,
+            };
         }
-
         return config;
     },
     (error) => {
-        console.error('Request error:', error);
+        console.error('请求拦截器错误:', error);
         return Promise.reject(error);
     }
 );
 
-// 响应拦截器
+// 响应拦截器，用于处理API响应
 service.interceptors.response.use(
     (response) => {
-        const { data = {}, code = 0 } = response?.data || {};
-        if (+code === 0) {
+        // console.log('响应数据:', response); 控制台输出响应数据
+        const { data, code } = response.data || {};
+        if (code === 0) {
             return data || {};
         } else {
-            console.error('Response error:', response?.data || {});
-            return Promise.reject(response?.data || {});
+            return Promise.reject(response.data || {});
         }
     },
     (error) => {
-        const { response } = error || {};
-        console.error('Response error:', response);
-
-        // 重定向
+        console.error('响应拦截器错误:', error); // 报错处理
+        const { response } = error;
         if (response?.status === 401) {
             localStorage.removeItem('PAOPAO_TOKEN');
-
             if (response?.data?.code !== 10005) {
                 window.$message.warning(response?.data?.msg || '鉴权失败');
             } else {
-                // 打开登录弹窗
                 window.$store.commit('triggerAuth', true);
             }
         } else {
@@ -60,5 +55,6 @@ service.interceptors.response.use(
 export default service;
 
 export function request<T, R>(config: AxiosRequestConfig<T>): Promise<R> {
+    console.log('发起请求:', config);
     return service(config) as unknown as Promise<R>;
 }
